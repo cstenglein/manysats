9d637a324e23cda28476e0e0367a93678ca1a8b9
Christoph Stenglein <christoph@cstenglein.com>
2022-09-09 09:46:41 +0200

    better styling, add btc icon

--- components/Main.tsx
+++ components/Main.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import { ChangeEvent, useEffect, useState } from "react";
 
 const Main = () => {
@@ -11,7 +12,6 @@ const Main = () => {
     const input = event.target.value.replace(/[^0-9.]/g, "");
     const formattedInput = input.endsWith(".") ? input : new Intl.NumberFormat("en-US").format(+input);
     setFiatAmount(formattedInput);
-    console.log("CHANGE FIAT", formattedInput);
     // format output
     const converted = ((+input / price!) * 100_000_000).toFixed(0);
     const formatted = new Intl.NumberFormat("en-US").format(+converted);
@@ -23,7 +23,6 @@ const Main = () => {
     const input = event.target.value.replace(/[^0-9.]/g, "");
     const formattedInput = input.endsWith(".") ? input : new Intl.NumberFormat("en-US").format(+input);
     setSatAmount(formattedInput);
-    console.log("CHANGE SAT", formattedInput);
     // format output
     const converted = ((+input * price!) / 100_000_000).toFixed(2);
     const formatted = new Intl.NumberFormat("en-US").format(+converted);
@@ -46,30 +45,42 @@ const Main = () => {
 
   return (
     <main className="flex h-screen flex-col items-center bg-gray-100">
-      <h1 className="mb-8 p-8 text-4xl">ManySats</h1>
-      <section className="flex w-11/12 flex-col items-center text-white shadow-xl md:w-auto">
+      <h1 className="mb-2 flex items-center justify-center p-4 text-4xl">
+        <Image src={"/btc-icon.svg"} height="40px" alt="btcicon" width="40px" /> <span className="ml-2">ManySats</span>
+      </h1>
+      <h6 className="mb-8 p-8 text-center text-2xl italic text-blue-500">Your simple Fiat to Satoshi Converter</h6>
+      <section className="flex w-11/12 flex-col items-center border border-blue-400 bg-white shadow-md md:w-auto">
+        {/* price pill */}
         <article className="mx-5 flex w-full justify-start px-2">
           <p className="z-10 -my-5 rounded-full bg-blue-500 p-2 text-sm text-white">{formattedPrice} € / BTC</p>
         </article>
-        <article className="flex w-full justify-center rounded-t-xl bg-blue-600 p-20">
+        {/* input 1 */}
+        <article className="flex w-full justify-center rounded-t-xl p-10">
           <input
-            className="border-b border-white bg-blue-600 p-2 outline-none focus:border-white"
+            id="input-fiat"
+            className="w-11/12 border-b border-blue-500 p-2 font-mono outline-none focus:shadow-[0_1px_0px_0px_rgba(59,130,246,1)]"
             type="text"
             inputMode="decimal"
             value={fiatAmount}
             onChange={onChangeFiatHandler}
           ></input>
-          <label className="p-2">€</label>
+          <label className="w-1/12 p-2" htmlFor="input-fiat">
+            €
+          </label>
         </article>
-        <article className="flex w-full justify-center rounded-b-xl bg-blue-700 p-20 ">
+        {/* input 2 */}
+        <article className="flex w-full justify-center rounded-b-xl p-10">
           <input
-            className="border-b border-white bg-blue-700 p-2 outline-none focus:border-white"
+            id="input-sat"
+            className="w-11/12 border-b border-blue-500 p-2 font-mono outline-none focus:shadow-[0_1px_0px_0px_rgba(59,130,246,1)]"
             type="text"
             inputMode="decimal"
             value={satAmount}
             onChange={onChangeSatHandler}
           ></input>
-          <label className="p-2">SATS</label>
+          <label className="w-1/12 p-2" htmlFor="input-sat">
+            SATS
+          </label>
         </article>
       </section>
     </main>
