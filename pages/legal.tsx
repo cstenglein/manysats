c7cae0607b0143231c2965cd615ba5b8689c18b8
Christoph Stenglein <christoph@cstenglein.com>
2022-09-10 18:08:21 +0200

    add basic legal page

---
+++ pages/legal.tsx
@@ -0,0 +1,7 @@
+import { NextPage } from "next";
+
+const Legal: NextPage = () => {
+  return <main className="flex h-screen flex-col items-center bg-gray-100">Legal</main>;
+};
+
+export default Legal;
