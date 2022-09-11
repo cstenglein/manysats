b5eb8639beb58a65d34d5495377abce9381aee30
Christoph Stenglein <christoph@cstenglein.com>
2022-09-11 09:59:27 +0200

    footer styling

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,7 +5,7 @@ const Footer: FC = () => {
   return (
     <footer className="absolute bottom-0 flex w-full justify-center text-sm">
       <Link href="/legal">
-        <span className="rounded-t bg-blue-500 p-2 text-white">Legal</span>
+        <span className="cursor-pointer rounded-t bg-blue-500 p-2 text-white">Legal</span>
       </Link>
     </footer>
   );
