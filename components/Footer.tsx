49e3a967c629fc435eff83baf59a0bcbad541f1f
Christoph Stenglein <christoph@cstenglein.com>
2022-09-12 17:06:15 +0200

    better styling for footer links

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,7 @@ import { FC } from "react";
 
 const Footer: FC = () => {
   return (
-    <footer className="absolute bottom-0 flex w-full justify-center text-sm">
+    <footer className="absolute bottom-0 flex w-full justify-center gap-2 rounded-t  text-sm">
       <Link href="/faq">
         <button className="cursor-pointer rounded-t bg-blue-500 p-2 text-white">FAQ</button>
       </Link>
