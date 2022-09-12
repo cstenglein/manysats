44087b171fbb2cbbfc2f50c21856f715bc42ba63
Christoph Stenglein <christoph@cstenglein.com>
2022-09-12 06:59:23 +0200

    add contact details to legal

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,7 +5,7 @@ const Footer: FC = () => {
   return (
     <footer className="absolute bottom-0 flex w-full justify-center text-sm">
       <Link href="/legal">
-        <span className="cursor-pointer rounded-t bg-blue-500 p-2 text-white">Legal</span>
+        <button className="cursor-pointer rounded-t bg-blue-500 p-2 text-white">Legal</button>
       </Link>
     </footer>
   );
--- pages/legal.tsx
+++ pages/legal.tsx
@@ -1,7 +1,31 @@
 import { NextPage } from "next";
+import Link from "next/link";
+
+const ADDRESS = `Christoph Stenglein
+Thalmannsbach 15
+4771 Sigharting
+manysats@cstenglein.com`;
 
 const Legal: NextPage = () => {
-  return <main className="flex h-screen flex-col items-center bg-gray-100">Legal</main>;
+  const onClickHandler = () => {
+    alert(ADDRESS);
+  };
+
+  return (
+    <main className="flex h-screen flex-col items-center bg-gray-100">
+      <h1 className="mb-2 flex items-center justify-center p-4 text-4xl">Legal</h1>
+
+      <section className="flex h-screen flex-col gap-4 pt-10">
+        <button className="rounded bg-blue-500 p-2 text-white" onClick={onClickHandler}>
+          Show Contact Details
+        </button>
+
+        <Link href="/">
+          <button className="rounded-t bg-blue-500 p-2 text-white">Back to Home</button>
+        </Link>
+      </section>
+    </main>
+  );
 };
 
 export default Legal;
