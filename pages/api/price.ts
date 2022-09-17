0114a2dc7785ad9087768973f489d8e00bd31163
Christoph Stenglein <christoph@cstenglein.com>
2022-09-17 16:57:55 +0200

    add caching in backend

--- pages/api/price.ts
+++ pages/api/price.ts
@@ -2,7 +2,7 @@ import type { NextApiRequest, NextApiResponse } from "next";
 
 type PriceData = {
   price: number | null;
-  date: string | null;
+  date: Date | null;
 };
 
 let cachedData: PriceData = {
@@ -10,15 +10,18 @@ let cachedData: PriceData = {
   date: null,
 };
 
+const CACHE_MS = 180_000;
+
 export default async function handler(_: NextApiRequest, res: NextApiResponse<any>) {
-  if (!cachedData.price) {
+  if (!cachedData.date || new Date().getTime() - cachedData.date?.getTime() > CACHE_MS) {
+    console.info("fetch data");
     const data = await fetch("https://api.kraken.com/0/public/Ticker?pair=XBTEUR");
     const json = await data.json();
 
     cachedData.price = +json.result.XXBTZEUR.a[0];
-    cachedData.date = new Date().toISOString();
+    cachedData.date = new Date();
   } else {
-    console.log("cached :)");
+    console.info("use cached data");
   }
 
   res.status(200).json(cachedData);
