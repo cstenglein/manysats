cd76d0bef7938d3094e01e898836a309db4a00a8
Christoph Stenglein <christoph@cstenglein.com>
2022-09-18 11:34:34 +0200

    add viewport meta tag

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,6 +9,7 @@ const Home: NextPage = () => {
       <Head>
         <title>ManySats</title>
         <meta name="description" content="Fiat to Satoshi Converter" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
         <link rel="manifest" href="/manifest.json" />
       </Head>
