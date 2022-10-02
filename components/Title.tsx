dba2d41bdbddecf635481d4c3659adaa7d765cb3
Christoph Stenglein <christoph@cstenglein.com>
2022-10-02 12:33:42 +0200

    add correct heading for Title; add better alt description for icon

--- components/Title.tsx
+++ components/Title.tsx
@@ -1,15 +1,14 @@
-import Link from "next/link";
-import { FC } from "react";
 import Image from "next/image";
+import { FC } from "react";
 
 const Title: FC = () => {
   return (
     <>
       <h1 className="mb-2 mt-4 flex items-center justify-center p-4 text-4xl">
-        <Image src={"/btc-icon.svg"} height="40px" alt="btcicon" width="40px" />
+        <Image src={"/btc-icon.svg"} height={40} alt="Bitcoin Icon" width={40} />
         <span className="ml-2">ManySats</span>
       </h1>
-      <h6 className="mb-8 p-8 text-center text-2xl italic text-blue-500">Your simple Fiat to Satoshi Converter</h6>
+      <h2 className="mb-8 p-8 text-center text-2xl italic text-blue-500">Your simple Fiat to Satoshi Converter</h2>
     </>
   );
 };
