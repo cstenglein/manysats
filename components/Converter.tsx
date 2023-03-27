e981a5a0d2c8cb376607a22a29539c55b73be344
Christoph Stenglein <christoph@cstenglein.com>
2023-03-27 15:48:15 +0200

    format numbers correctly with react-number-format

--- components/Converter.tsx
+++ components/Converter.tsx
@@ -1,6 +1,7 @@
 import { ChangeEvent, FC, useEffect, useRef, useState } from "react";
-import { convert } from "../utils/convert";
-import { getFormatter, getSeparator } from "../utils/utils";
+import { NumericFormat } from "react-number-format";
+import { convertBtcToFiat, convertFiatToBtc } from "../utils/convert";
+import { getSeparator } from "../utils/utils";
 import CurrentPrice from "./CurrentPrice";
 import RefreshBtn from "./RefreshBtn";
 
@@ -21,7 +22,6 @@ const Converter: FC<Props> = ({ price, formattedPrice, onRefresh }) => {
   const [decimalSep, setDecimalSep] = useState<string>("");
   const [selectedUnit, setSelectedUnit] = useState<Unit>(Unit.SAT);
 
-  const regex = new RegExp(`[^0-9${decimalSep}]`, "g");
   const inputFiat = useRef<HTMLInputElement | null>(null);
 
   const onChangeFiatHandler = (event: ChangeEvent<HTMLInputElement>) => {
@@ -29,27 +29,9 @@ const Converter: FC<Props> = ({ price, formattedPrice, onRefresh }) => {
     if (!price) {
       return;
     }
-    let input = event.target.value.replace(regex, "");
 
-    // cut off third decimal place
-    if (input.indexOf(decimalSep) > 0) {
-      input = input.slice(0, input.indexOf(decimalSep) + 3);
-    }
-
-    let floatInput = parseFloat(input.replace(decimalSep, ".")) || 0;
-
-    // format input
-    const INPUT_FORMATTER = getFormatter(window.navigator.language, Unit.SAT);
-    // if input contains a separator (e.g. "200.") then format and add separator again for further input
-    const formattedInput = input.endsWith(decimalSep)
-      ? `${INPUT_FORMATTER.format(floatInput)}${decimalSep}`
-      : INPUT_FORMATTER.format(floatInput);
-    setFiatAmount(formattedInput);
-
-    // format output
-    const OUTPUT_FORMATTER = getFormatter(window.navigator.language, selectedUnit);
-    const formatted = OUTPUT_FORMATTER.format(+convert(floatInput, price!, selectedUnit));
-    setSatAmount(formatted);
+    setFiatAmount(event.target.value);
+    setSatAmount(convertFiatToBtc(+event.target.value.replace(/,/g, ""), price!, selectedUnit));
   };
 
   const onChangeSatHandler = (event: ChangeEvent<HTMLInputElement>) => {
@@ -57,27 +39,13 @@ const Converter: FC<Props> = ({ price, formattedPrice, onRefresh }) => {
     if (!price) {
       return;
     }
-    const FORMATTER = getFormatter(window.navigator.language, selectedUnit);
 
-    // format input
-    const input = parseFloat(event.target.value.replace(/[^0-9]/g, "")) || 0;
-    const formattedInput = FORMATTER.format(input);
-    setSatAmount(formattedInput);
-
-    // format output
-    const converted = ((input * price) / 100_000_000).toFixed(2);
-    const formatted = FORMATTER.format(+converted);
-    setFiatAmount(formatted);
+    setSatAmount(event.target.value);
+    setFiatAmount(convertBtcToFiat(+event.target.value.replace(/,/g, ""), price!, selectedUnit));
   };
 
   const onChangeSelect = (e: ChangeEvent<HTMLSelectElement>) => {
-    let input = fiatAmount.replace(regex, "");
-    let floatInput = parseFloat(input.replace(decimalSep, ".")) || 0;
-
-    const FORMATTER = getFormatter(window.navigator.language, +e.target.value);
-
-    const formatted = FORMATTER.format(+convert(floatInput, price!, e.target.value as unknown as Unit));
-    setSatAmount(formatted);
+    setSatAmount(convertFiatToBtc(+fiatAmount.replace(/,/g, ""), price!, e.target.value as unknown as Unit));
     setSelectedUnit(e.target.value as unknown as Unit);
   };
 
@@ -94,29 +62,27 @@ const Converter: FC<Props> = ({ price, formattedPrice, onRefresh }) => {
       <RefreshBtn onRefresh={onRefresh} />
       {/* input 1 */}
       <article className="flex w-full justify-center rounded-t-xl p-10">
-        <input
-          ref={inputFiat}
+        <NumericFormat
           id="input-fiat"
           className="input-underline"
-          type="text"
-          inputMode="decimal"
           value={fiatAmount}
+          thousandSeparator=","
+          getInputRef={inputFiat}
           onChange={onChangeFiatHandler}
-        ></input>
+        />
         <label className="w-1/12 p-2" htmlFor="input-fiat">
           €
         </label>
       </article>
       {/* input 2 */}
       <article className="flex w-full items-center justify-center gap-2 rounded-b-xl p-10">
-        <input
+        <NumericFormat
           id="input-sat"
           className="input-underline"
-          type="text"
-          inputMode="decimal"
           value={satAmount}
+          thousandSeparator=","
           onChange={onChangeSatHandler}
-        ></input>
+        />
         <select className="bg-white" value={selectedUnit} onChange={onChangeSelect}>
           <option value={Unit.SAT}>SAT</option>
           <option value={Unit.BTC}>BTC</option>
--- utils/convert.ts
+++ utils/convert.ts
@@ -1,6 +1,6 @@
 import { Unit } from "../components/Converter";
 
-export function convert(floatInput: number, price: number, selectedUnit: Unit) {
+export function convertFiatToBtc(floatInput: number, price: number, selectedUnit: Unit) {
   let converted: string;
   switch (+selectedUnit as Unit) {
     case Unit.SAT:
@@ -8,6 +8,19 @@ export function convert(floatInput: number, price: number, selectedUnit: Unit) {
       break;
     case Unit.BTC:
       converted = (floatInput / price).toFixed(8);
+      break;
+  }
+  return converted;
+}
+
+export function convertBtcToFiat(btcInput: number, price: number, selectedUnit: Unit) {
+  let converted: string;
+  switch (+selectedUnit as Unit) {
+    case Unit.SAT:
+      converted = ((btcInput * price) / 100_000_000).toFixed(2);
+      break;
+    case Unit.BTC:
+      converted = (btcInput * price).toFixed(2);
       break;
   }
   return converted;
