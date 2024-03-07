a15b992da972ec9bca47b77cb5faf5a5df87a2c0
Christoph Stenglein <christoph@cstenglein.com>
2024-03-07 16:41:30 +0100

    add lightning address for tips

--- app/page.tsx
+++ app/page.tsx
@@ -10,6 +10,8 @@ export default function Home() {
       <Link href="/imprint" className="mt-5 flex w-full justify-center gap-2 text-sm">
         <button className="cursor-pointer rounded bg-blue-600 p-2 text-white">Imprint</button>
       </Link>
+      <p className="p-2 text-blue-600">Lightning Address for tips:</p>
+      <span className="text-blue-600 underline">christoph@getalby.com</span>
     </main>
   );
 }
